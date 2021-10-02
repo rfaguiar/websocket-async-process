cbe9d3d38bde7fdd90d6bda5c01e8fde73e45d4e
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2021-10-01 21:47:54 -0300

    implements app-bff project with reactjs for show input username and get token button for send post api to create token and reseive solicitation id and show into solicitations table

---
+++ app-bff/src/App.tsx
@@ -0,0 +1,62 @@
+import React, {useEffect, useState} from 'react';
+import './App.css';
+import axios from "axios";
+
+function App() {
+
+  const [username, setUsername] = useState('');
+  const [tableData, setTableData] = useState<string[]>([]);
+
+  useEffect(() => {
+    console.log('tableData', tableData);
+  }, [tableData]);
+
+  async function getToken() {
+    let axiosResponse = await axios.post('/api/token', {username: username});
+    let message = "Received solicitationId: " + axiosResponse.data.solicitationId;
+    setTableData(tableData => [...tableData, message]);
+  }
+
+  return (
+      <div id="main-content" className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <form className="form-inline" onSubmit={event => {
+              event.preventDefault();
+              getToken();
+            }}>
+              <div className="form-group">
+                <label htmlFor="name">What is your username?</label>
+                <input type="text" id="name" onChange={e => setUsername(e.target.value)} className="form-control" placeholder="Your username here..."/>
+              </div>
+              <button id="send" className="btn btn-default" type="submit">Get Token</button>
+            </form>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-12">
+            <table id="conversation" className="table table-striped">
+              <thead>
+              <tr>
+                <th>Solicitations</th>
+              </tr>
+              </thead>
+              <tbody id="solicitations">
+              {
+                tableData.map((message, index) => {
+                  return (
+                      <tr key={index}>
+                        <td className='white-space:nowrap;word-wrap:break-word;'>{message}</td>
+                      </tr>
+                  )
+                })
+              }
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+  );
+}
+
+export default App;
